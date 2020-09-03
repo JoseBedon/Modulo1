827026b6f7f9df7e6d3105adeb88802cc4919b6a
JoseBedon <josebedon@live.com>
2020-09-03 09:15:50 -0500

    curso modulo1
    taller

---
+++ SNGridlayout/SNGridlayout/SNGridlayout/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using SNGridlayout.Views;
+
+namespace SNGridlayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new Home();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
